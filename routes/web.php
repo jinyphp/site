f99eede398d2e22e8d4e4814a2eaf34139177521
hojinlee <infohojin@gmail.com>
2025-10-17 23:20:40 +0900

    service

--- routes/web.php
+++ routes/web.php
@@ -121,20 +121,6 @@ Route::middleware('web')->prefix('event')->name('event.')->group(function () {
         ->where('identifier', '[a-zA-Z0-9\-_]+');
 });
 
-// FAQ (site-cms에서 이동)
-Route::middleware('web')->prefix('faq')->name('faq.')->group(function () {
-    // FAQ 메인 페이지
-    Route::get('/', \Jiny\Site\Http\Controllers\Site\Faq\IndexController::class)
-        ->name('index');
-
-    // FAQ 검색
-    Route::get('/search', \Jiny\Site\Http\Controllers\Site\Faq\SearchController::class)
-        ->name('search');
-
-    // FAQ 카테고리별 목록
-    Route::get('/category/{code}', \Jiny\Site\Http\Controllers\Site\Faq\CategoryController::class)
-        ->name('category');
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -244,10 +230,11 @@ Route::middleware('web')->prefix('testimonials')->name('testimonials.')->group(f
 
 
     // 동적 페이지 (Fallback - 마지막에 위치)
-    // cart, product, contact, help, faq, event, board, testimonials 경로는 제외
+    // cart, product, contact, help, faq, event, testimonials 경로는 제외
+    // board는 jiny/post 패키지에서 처리됨
     Route::get('/{slug}', [\Jiny\Site\Http\Controllers\PageController::class, 'show'])
         ->name('page.show')
-        ->where('slug', '^(?!cart|product|contact|help|faq|event|board|testimonials)([a-zA-Z0-9\-_]+)$');
+        ->where('slug', '^(?!cart|product|contact|help|faq|event|testimonials)([a-zA-Z0-9\-_]+)$');
 
 // Sitemap & RSS
 Route::middleware('web')->group(function () {
@@ -260,193 +247,7 @@ Route::middleware('web')->group(function () {
         ->name('rss.xml');
 });
 
-// Help (site-cms에서 이동)
-Route::middleware('web')->prefix('help')->name('help.')->group(function () {
-    // Help 메인 페이지
-    Route::get('/', \Jiny\Site\Http\Controllers\Site\Help\IndexController::class)
-        ->name('index');
 
-    // Help 검색
-    Route::get('/search', \Jiny\Site\Http\Controllers\Site\Help\SearchController::class)
-        ->name('search');
-
-    // Help FAQ
-    Route::get('/faq', \Jiny\Site\Http\Controllers\Site\Help\Faq\IndexController::class)
-        ->name('faq');
-
-    // Help 가이드
-    Route::get('/guide', \Jiny\Site\Http\Controllers\Site\Help\Guide\IndexController::class)
-        ->name('guide');
-
-    // Help 가이드 상세
-    Route::get('/guide/{id}', \Jiny\Site\Http\Controllers\Site\Help\Guide\ShowController::class)
-        ->name('guide.single')
-        ->where('id', '[0-9]+');
-
-    // Help 가이드 좋아요
-    Route::post('/guide/{id}/like', \Jiny\Site\Http\Controllers\Site\Help\Guide\LikeController::class)
-        ->name('guide.like')
-        ->where('id', '[0-9]+');
-
-    // Help 고객지원
-    Route::match(['GET', 'POST'], '/support', \Jiny\Site\Http\Controllers\Site\Help\Support\IndexController::class)
-        ->name('support');
-
-    // 지원 요청 성공 페이지
-    Route::get('/support/success', \Jiny\Site\Http\Controllers\Site\Help\Support\SuccessController::class)
-        ->name('support.success');
-
-    // 내 지원 요청 목록 (로그인 필요)
-    Route::get('/support/my', \Jiny\Site\Http\Controllers\Site\Help\Support\MyController::class)
-        ->name('support.my')
-        ->middleware('auth');
-
-    // 내 지원 요청 상세 보기 (로그인 필요)
-    Route::get('/support/my/{id}', \Jiny\Site\Http\Controllers\Site\Help\Support\ShowController::class)
-        ->name('support.show')
-        ->middleware('auth')
-        ->where('id', '[0-9]+');
-
-    // 지원 요청 평가 제출 (로그인 필요)
-    Route::post('/support/{id}/evaluate', \Jiny\Site\Http\Controllers\Site\Help\Support\EvaluationController::class)
-        ->name('support.evaluate')
-        ->middleware('auth')
-        ->where('id', '[0-9]+');
-
-    // 사용자 직접 지원 요청 종료 (로그인 필요)
-    Route::post('/support/{id}/close', \Jiny\Site\Http\Controllers\Site\Help\Support\CloseController::class)
-        ->name('support.close')
-        ->where('id', '[0-9]+');
-
-    // 고객 추가 문의 제출 (로그인 필요)
-    Route::post('/support/{id}/reply', \Jiny\Site\Http\Controllers\Site\Help\Support\CustomerReplyController::class)
-        ->name('support.customer.reply')
-        ->middleware('auth')
-        ->where('id', '[0-9]+');
-
-    // 지원 요청 수정 (로그인 필요)
-    Route::match(['GET', 'POST'], '/support/{id}/edit', \Jiny\Site\Http\Controllers\Site\Help\Support\EditController::class)
-        ->name('support.edit')
-        ->middleware('auth')
-        ->where('id', '[0-9]+');
-
-    // 지원 요청 삭제 (로그인 필요)
-    Route::delete('/support/{id}/delete', \Jiny\Site\Http\Controllers\Site\Help\Support\DeleteController::class)
-        ->name('support.delete')
-        ->middleware('auth')
-        ->where('id', '[0-9]+');
-
-    // Help 카테고리별 목록
-    Route::get('/category/{code}', \Jiny\Site\Http\Controllers\Site\Help\CategoryController::class)
-        ->name('category');
-
-    // Help 상세 페이지
-    Route::get('/{id}', \Jiny\Site\Http\Controllers\Site\Help\DetailController::class)
-        ->name('detail')
-        ->where('id', '[0-9]+');
-
-    // Help 좋아요 기능
-    Route::post('/{id}/like', function($id) {
-        // 좋아요 처리 로직
-        DB::table('site_help')->where('id', $id)->increment('like');
-        return response()->json(['success' => true]);
-    })->name('like')
-      ->where('id', '[0-9]+');
-});
-
-/*
-|--------------------------------------------------------------------------
-| Board Routes (회원용 게시판)
-|--------------------------------------------------------------------------
-|
-| 회원이 접근 가능한 게시판 기능을 제공합니다.
-| 게시글 작성/수정/삭제는 로그인한 사용자만 가능하며,
-| 자신의 게시글만 수정/삭제할 수 있습니다.
-|
-*/
-
-Route::middleware('web')->prefix('board')->name('board.')->group(function () {
-    // 게시판 대시보드 (메인 페이지)
-    Route::get('/', \Jiny\Site\Http\Controllers\Site\Board\DashboardController::class)
-        ->name('dashboard');
-
-    // 게시판 목록
-    Route::get('/{code}', \Jiny\Site\Http\Controllers\Site\Board\IndexController::class)
-        ->name('index');
-
-    // 게시글 상세보기
-    Route::get('/{code}/{id}', \Jiny\Site\Http\Controllers\Site\Board\ShowController::class)
-        ->name('show')
-        ->where('id', '[0-9]+');
-
-    // 새 글 작성
-    Route::get('/{code}/create', \Jiny\Site\Http\Controllers\Site\Board\CreateController::class)
-        ->name('create');
-
-    Route::post('/{code}', \Jiny\Site\Http\Controllers\Site\Board\StoreController::class)
-        ->name('store');
-
-    // 하위글 작성
-    Route::get('/{code}/{id}/reply', \Jiny\Site\Http\Controllers\Site\Board\CreateChildController::class)
-        ->name('reply')
-        ->where('id', '[0-9]+');
-
-    // 글 수정
-    Route::get('/{code}/{id}/edit', \Jiny\Site\Http\Controllers\Site\Board\EditController::class)
-        ->name('edit')
-        ->where('id', '[0-9]+');
-
-    Route::put('/{code}/{id}', \Jiny\Site\Http\Controllers\Site\Board\UpdateController::class)
-        ->name('update')
-        ->where('id', '[0-9]+');
-
-    // 글 삭제
-    Route::delete('/{code}/{id}', \Jiny\Site\Http\Controllers\Site\Board\DestroyController::class)
-        ->name('destroy')
-        ->where('id', '[0-9]+');
-
-    // 코멘트 저장
-    Route::post('/{code}/{id}/comment', \Jiny\Site\Http\Controllers\Site\Board\StoreCommentController::class)
-        ->name('comment.store')
-        ->where('id', '[0-9]+');
-
-    // 코멘트 수정
-    Route::put('/{code}/{id}/comment/{commentId}', \Jiny\Site\Http\Controllers\Site\Board\UpdateCommentController::class)
-        ->name('comment.update')
-        ->where(['id' => '[0-9]+', 'commentId' => '[0-9]+']);
-
-    // 코멘트 삭제
-    Route::delete('/{code}/{id}/comment/{commentId}', \Jiny\Site\Http\Controllers\Site\Board\DestroyCommentController::class)
-        ->name('comment.destroy')
-        ->where(['id' => '[0-9]+', 'commentId' => '[0-9]+']);
-
-    // 게시글 평가 (좋아요/별점)
-    Route::post('/{code}/{id}/rating', \Jiny\Site\Http\Controllers\Site\Board\StoreRatingController::class)
-        ->name('rating.store')
-        ->where('id', '[0-9]+');
-
-    // UUID 기반 라우트 (샤딩 환경 지원)
-    Route::prefix('uuid')->name('uuid.')->group(function () {
-        // UUID로 게시글 상세보기
-        Route::get('/{code}/{uuid}', \Jiny\Site\Http\Controllers\Site\Board\ShowController::class)
-            ->name('show')
-            ->where('uuid', '[0-9a-f-]{36}');
-
-        // UUID로 글 수정
-        Route::get('/{code}/{uuid}/edit', \Jiny\Site\Http\Controllers\Site\Board\EditController::class)
-            ->name('edit')
-            ->where('uuid', '[0-9a-f-]{36}');
-
-        Route::put('/{code}/{uuid}', \Jiny\Site\Http\Controllers\Site\Board\UpdateController::class)
-            ->name('update')
-            ->where('uuid', '[0-9a-f-]{36}');
-
-        // UUID로 글 삭제
-        Route::delete('/{code}/{uuid}', \Jiny\Site\Http\Controllers\Site\Board\DestroyController::class)
-            ->name('destroy')
-            ->where('uuid', '[0-9a-f-]{36}');
-    });
-});
 
 /*
 |--------------------------------------------------------------------------
